6c4d77f35b3d53ba6980ddc3b34ee8a54cba12b4
Simon Murray <spjmurray@yahoo.co.uk>
2024-02-02 14:40:59 +0000

    Update Server Openstack Interface
    We are moving away from OpenStack being the only provider, so make it so
    the helm/CLI interfaces are unambiguous and properly scoped.  Refactor
    flags so they live closer to where they will be used to stop abuse.  Add
    in half of the support for unikorn application credentials, rest will
    come next week!

--- pkg/providers/openstack/image.go
+++ pkg/providers/openstack/image.go
@@ -26,21 +26,35 @@ import (
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
 	"github.com/gophercloud/gophercloud/openstack/imageservice/v2/images"
+	"github.com/spf13/pflag"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 	"golang.org/x/exp/slices"
 
 	"github.com/unikorn-cloud/core/pkg/constants"
 	"github.com/unikorn-cloud/core/pkg/util"
+	"github.com/unikorn-cloud/unikorn/pkg/flags"
 )
+
+// ImageOptions allows things like filtering to be configured.
+type ImageOptions struct {
+	Properties []string
+	SigningKey flags.PublicKeyVar
+}
+
+func (o *ImageOptions) AddFlags(f *pflag.FlagSet) {
+	f.StringSliceVar(&o.Properties, "openstack-image-properties", nil, "Properties an image must possess in order to be exposed")
+	f.Var(&o.SigningKey, "openstack-image-signing-key", "Public key used to verify valid images for use with the platform")
+}
 
 // ImageClient wraps the generic client because gophercloud is unsafe.
 type ImageClient struct {
-	client *gophercloud.ServiceClient
+	client  *gophercloud.ServiceClient
+	options *ImageOptions
 }
 
 // NewImageClient provides a simple one-liner to start computing.
-func NewImageClient(provider Provider) (*ImageClient, error) {
+func NewImageClient(provider Provider, options *ImageOptions) (*ImageClient, error) {
 	providerClient, err := provider.Client()
 	if err != nil {
 		return nil, err
@@ -52,14 +66,15 @@ func NewImageClient(provider Provider) (*ImageClient, error) {
 	}
 
 	c := &ImageClient{
-		client: client,
+		client:  client,
+		options: options,
 	}
 
 	return c, nil
 }
 
-func validateProperties(image *images.Image, required []string) bool {
-	for _, r := range required {
+func (c *ImageClient) validateProperties(image *images.Image) bool {
+	for _, r := range c.options.Properties {
 		if !slices.Contains(util.Keys(image.Properties), r) {
 			return false
 		}
@@ -69,12 +84,12 @@ func validateProperties(image *images.Image, required []string) bool {
 }
 
 // verifyImage asserts the image is trustworthy for use with our goodselves.
-func verifyImage(image *images.Image, key *ecdsa.PublicKey) bool {
+func (c *ImageClient) verifyImage(image *images.Image) bool {
 	if image.Properties == nil {
 		return false
 	}
 
-	if key != nil {
+	if c.options.SigningKey.Key != nil {
 		// These will be digitally signed by Baski when created, so we only trust
 		// those images.
 		signatureRaw, ok := image.Properties["digest"]
@@ -94,14 +109,14 @@ func verifyImage(image *images.Image, key *ecdsa.PublicKey) bool {
 
 		hash := sha256.Sum256([]byte(image.ID))
 
-		return ecdsa.VerifyASN1(key, hash[:], signature)
+		return ecdsa.VerifyASN1(c.options.SigningKey.Key, hash[:], signature)
 	}
 
 	return true
 }
 
 // Images returns a list of images.
-func (c *ImageClient) Images(ctx context.Context, key *ecdsa.PublicKey, properties []string) ([]images.Image, error) {
+func (c *ImageClient) Images(ctx context.Context) ([]images.Image, error) {
 	tracer := otel.GetTracerProvider().Tracer(constants.Application)
 
 	_, span := tracer.Start(ctx, "/imageservice/v2/images", trace.WithSpanKind(trace.SpanKindClient))
@@ -127,13 +142,11 @@ func (c *ImageClient) Images(ctx context.Context, key *ecdsa.PublicKey, properti
 			continue
 		}
 
-		if properties != nil {
-			if !validateProperties(&image, properties) {
-				continue
-			}
+		if !c.validateProperties(&image) {
+			continue
 		}
 
-		if !verifyImage(&image, key) {
+		if !c.verifyImage(&image) {
 			continue
 		}
 
