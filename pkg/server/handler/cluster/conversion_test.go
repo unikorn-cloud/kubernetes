7539658e296a744931e9ea06bc28df41aa8ec445
Simon Murray <spjmurray@yahoo.co.uk>
2025-04-16 12:04:30 +0100

    Reinstate Auto-Upgrade API
    While our hearts ar in the right place (e.g. keep things up to date and
    secure), certain users are pests and will want absolute control over
    when upgrades happen, so propagate the correct information.  This will
    need a UI implementation to ensure it doesn't impose too much visual
    clutter and the APIs are actually usable in typescript.
    
    Fixes #211

---
+++ pkg/server/handler/cluster/conversion_test.go
@@ -0,0 +1,188 @@
+/*
+Copyright 2025 the Unikorn Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//nolint:testpackage
+package cluster
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/stretchr/testify/require"
+
+	unikorncorev1 "github.com/unikorn-cloud/core/pkg/apis/unikorn/v1alpha1"
+	unikornv1 "github.com/unikorn-cloud/kubernetes/pkg/apis/unikorn/v1alpha1"
+	"github.com/unikorn-cloud/kubernetes/pkg/openapi"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/ptr"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+const (
+	versionOldest = "v1.0.0"
+	versionMiddle = "v1.1.0"
+	versionNewest = "v1.1.2"
+)
+
+func bundleName(version string) string {
+	return "bundle-" + version
+}
+
+// applicationBundleFixture generates an application bundle object in a terse fashion.
+func applicationBundleFixture(t *testing.T, version string) *unikornv1.KubernetesClusterApplicationBundle {
+	t.Helper()
+
+	v, err := semver.NewVersion(version)
+	require.NoError(t, err)
+
+	return &unikornv1.KubernetesClusterApplicationBundle{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: bundleName(version),
+		},
+		Spec: unikornv1.ApplicationBundleSpec{
+			Version: unikorncorev1.SemanticVersion{
+				Version: *v,
+			},
+		},
+	}
+}
+
+// newClient creates a client with some bundle objects of various versions.
+func newClient(t *testing.T) client.Client {
+	t.Helper()
+
+	scheme := runtime.NewScheme()
+
+	require.NoError(t, unikornv1.AddToScheme(scheme))
+
+	bundles := []client.Object{
+		applicationBundleFixture(t, versionMiddle),
+		applicationBundleFixture(t, versionNewest),
+		applicationBundleFixture(t, versionOldest),
+	}
+
+	return fake.NewClientBuilder().WithScheme(scheme).WithObjects(bundles...).Build()
+}
+
+// TestApplicationBundleNameGenerationCreateDefault ensure the latest bundle is selected if none
+// is passed via the API.  This is lagacy behaviour.
+func TestApplicationBundleNameGenerationCreateDefault(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	c := newClient(t)
+
+	g := newGenerator(c, nil, nil, "", "", "")
+
+	request := &openapi.KubernetesClusterWrite{
+		Spec: openapi.KubernetesClusterSpec{},
+	}
+
+	name, err := g.generateApplicationBundleName(ctx, request)
+	require.NoError(t, err)
+	require.NotNil(t, name)
+	require.Equal(t, bundleName(versionNewest), *name)
+}
+
+// TestApplicationBundleNameGenerationCreateExplicit checks that a bundle is selected explicitly
+// overriding and defaults.
+func TestApplicationBundleNameGenerationCreateExplicit(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	c := newClient(t)
+
+	g := newGenerator(c, nil, nil, "", "", "")
+
+	request := &openapi.KubernetesClusterWrite{
+		Spec: openapi.KubernetesClusterSpec{
+			ApplicationBundleName: ptr.To(bundleName(versionMiddle)),
+		},
+	}
+
+	name, err := g.generateApplicationBundleName(ctx, request)
+	require.NoError(t, err)
+	require.NotNil(t, name)
+	require.Equal(t, bundleName(versionMiddle), *name)
+}
+
+// TestApplicationBundleNameGenerationUpdateDefault checks that on update a bundle version
+// is preserved and not accidentally upgraded.
+func TestApplicationBundleNameGenerationUpdateDefault(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	c := newClient(t)
+
+	existing := &unikornv1.KubernetesCluster{
+		Spec: unikornv1.KubernetesClusterSpec{
+			ApplicationBundle: ptr.To(bundleName(versionMiddle)),
+		},
+	}
+
+	g := newGenerator(c, nil, nil, "", "", "").withExisting(existing)
+
+	request := &openapi.KubernetesClusterWrite{
+		Spec: openapi.KubernetesClusterSpec{},
+	}
+
+	name, err := g.generateApplicationBundleName(ctx, request)
+	require.NoError(t, err)
+	require.NotNil(t, name)
+	require.Equal(t, bundleName(versionMiddle), *name)
+}
+
+// TestApplicationBundleNameGenerationUpdateExplicit checks that a bundle selected at the API
+// overrides any existing value.
+func TestApplicationBundleNameGenerationUpdateExplicit(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	c := newClient(t)
+
+	existing := &unikornv1.KubernetesCluster{
+		Spec: unikornv1.KubernetesClusterSpec{
+			ApplicationBundle: ptr.To(bundleName(versionNewest)),
+		},
+	}
+
+	g := newGenerator(c, nil, nil, "", "", "").withExisting(existing)
+
+	request := &openapi.KubernetesClusterWrite{
+		Spec: openapi.KubernetesClusterSpec{
+			ApplicationBundleName: ptr.To(bundleName(versionOldest)),
+		},
+	}
+
+	name, err := g.generateApplicationBundleName(ctx, request)
+	require.NoError(t, err)
+	require.NotNil(t, name)
+	require.Equal(t, bundleName(versionOldest), *name)
+}
